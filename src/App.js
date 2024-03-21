1b94e309e90815ab17894e659006b767083f0bd3
RyanFin <ryanfinlayson125@hotmail.com>
2024-03-21 21:22:16 +0000

    ensure component renders and disappears when button is pressed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-export default function App(){
+export default function App() {
+  // const step = 1;
+  // create a state variable
 
-  const step = 1;
-  
-  return (<div className="steps">
-    <div  className="numbers">
-      <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-      <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-      <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+  const [step, setStep] = useState(1); //default value 1
+  const [isOpen, setIsOpen] = useState(true);
+
+  console.log(step);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      // use setstep to update the step state variable
+      setStep(step + 1);
+    }
+  }
+
+  return (
+    <div>
+      {/* set isOpen to the opposite of what it currently is. ! is the negation operator in standard JS  */}
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+
+      {/* Conditional Rendering here determines if the code within the div is displayed if 'isOpen' is true */}
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-
-    <p className="message">Step {step}: {messages[step-1]}</p>
-
-    <div className="buttons">
-      <button style={{backgroundColor: "#7950f2", color:'#fff'}}>Previous</button>
-      <button style={{backgroundColor: "#7950f2", color:'#fff'}}>Next</button>
-    </div>
-  </div>);
+  );
 }
